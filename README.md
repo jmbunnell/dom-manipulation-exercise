f7d1c6ec722c4a881a16a248bb53dc5863830a91
JB <jacobbunnellca@gmail.com>
2022-08-26 10:14:47 -0400

    All DOM Exercises Complete!

--- README.md
+++ README.md
@@ -4,10 +4,10 @@
 
 2) an <h3> with blue text that says “I’m a blue h3!” - DONE
 
-3) a <div> with a black border and pink background color with the following elements inside of it:
+3) a <div> with a black border and pink background color with the following elements inside of it: - DONE
     
-    a) another <h1> that says “I’m in a div”
+    a) another <h1> that says “I’m in a div” - DONE
 
-    b) a <p> that says “ME TOO!”
+    b) a <p> that says “ME TOO!” - DONE
 
     c) Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.
--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const body = document.body;
 
 const para = document.createElement('p');
 para.textContent = "Hey I'm red!";
-body.appendChild(para);
 para.style.color = 'red'; 
+body.appendChild(para);
 
-const header = document.createElement('h3');
-header.textContent = "I'm a blue h3!";
-body.appendChild(header);
-header.style.color = 'blue';
+const head = document.createElement('h3');
+head.textContent = "I'm a blue h3!";
+head.style.color = 'blue';
+body.appendChild(head);
+
+const div = document.createElement('div');
+div.setAttribute('style', 'border: solid; border-color: black; background: pink');
+body.appendChild(div);
+
+const header = document.createElement('h1');
+header.textContent = "I'm in a div";
+div.appendChild(header);
+
+const paraTwo = document.createElement('p');
+paraTwo.textContent = "ME TOO!";
+div.appendChild(paraTwo, header);
